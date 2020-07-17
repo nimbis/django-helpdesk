e0d38159c1c53133c007b10bb3b4aadcca8902a3
Kevin Robbins <kevin.robbins99@gmail.com>
2020-07-17 11:50:51 -0400

    Handle new user creation in email_address_update signal
    Because this is a pre_save signal, if a user is about to be created,
    there is no user to query for and an exception is thrown. Here we change
    the get() to a filter() and check to see if a user was returned. If no
    user is found, then we can just return because they would not have any
    tickets to update.

--- helpdesk/signals.py
+++ helpdesk/signals.py
@@ -10,7 +10,11 @@ from .models import Ticket
 
 @receiver(pre_save, sender=User)
 def email_address_update(sender, instance, **kwargs):
-    pre_save_user = User.objects.get(id=instance.id)
+    qs = User.objects.filter(id=instance.id)
+    if not qs:
+        return
+
+    pre_save_user = qs.first()
     tickets = Ticket.objects.filter(submitter_email=pre_save_user.email)
     for ticket in tickets:
         ticket.submitter_email = instance.email
