e70daccde1106756b6ec6a25715902d45ea1ab1c
Kevin Robbins <kevin.robbins99@gmail.com>
2020-08-25 14:28:56 -0400

    Exit update_ticket_submitter_email early if email address didn't change
    We don't need to update the ticket if the email address didn't change so
    we can exit early. Here we also condense the early exit logic so that it
    all happens in a single, unbroken chunk.

--- helpdesk/signals.py
+++ helpdesk/signals.py
@@ -10,11 +10,16 @@ from .models import Ticket
 
 @receiver(pre_save, sender=User)
 def update_ticket_submitter_email(sender, instance, **kwargs):
-    qs = User.objects.filter(id=instance.id)
-    if not qs:
+    pre_save_user = User.objects.filter(id=instance.id).first()
+
+    # If the user doesn't exist yet, then they won't have any tickets
+    if not pre_save_user:
         return
 
-    pre_save_user = qs.first()
+    # Make sure the email address actually changed
+    if pre_save_user.email == instance.email:
+        return
+
     tickets = Ticket.objects.filter(submitter_email=pre_save_user.email)
     for ticket in tickets:
         ticket.submitter_email = instance.email
